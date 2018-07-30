2beeea1d8d882df1e687b5908ae5b8157968ba7d
Victor Loh <vic94loh@hotmail.com>
2018-07-30 09:18:49 +0800

    added equality check for WTypes

--- lang/wenttype.go
+++ lang/wenttype.go
@@ -8,7 +8,8 @@ import (
 // WType is an interface where all other `went` language data structures
 // should implemented, null is not within these types
 type WType interface {
-	IsZeroValue() WBool
+	IsZeroValue() WBool // returns true if the value is zero value
+	Equals(WType) WBool // returns true if the object compared to it is equals
 	String() string
 }
 
@@ -17,14 +18,31 @@ type WNull struct{}
 
 // IsZeroValue always returns true for null type
 func (w WNull) IsZeroValue() WBool { return true }
-func (w WNull) String() string     { return "null" }
+
+// Equals checks if the type compared to is equal
+func (w WNull) Equals(w2 WType) WBool {
+	if _, ok := w2.(WNull); ok {
+		return true
+	}
+	return false
+}
+func (w WNull) String() string { return "null" }
 
 // WNum is a number type in went, it combines both integers as well as floats
 type WNum float64
 
 // IsZeroValue returns the zero value of a went number value
 func (w WNum) IsZeroValue() WBool { return w == 0 }
-func (w WNum) String() string     { return fmt.Sprintf("%v", float64(w)) }
+
+// Equals checks if the type compared to is equal
+func (w WNum) Equals(w2 WType) WBool {
+	if v, ok := w2.(WNum); ok {
+		return w == v
+	}
+	return false
+}
+
+func (w WNum) String() string { return fmt.Sprintf("%v", float64(w)) }
 
 // IsInt checks if WNum is an integer, if not its a float
 func (w WNum) IsInt() bool { return float64(w) == float64(int64(w)) }
@@ -34,21 +52,56 @@ type WString string
 
 // IsZeroValue returns the zero value of a went string value
 func (w WString) IsZeroValue() WBool { return w == "" }
-func (w WString) String() string     { return fmt.Sprintf("'%v'", string(w)) }
+
+// Equals checks if the type compared to is equal
+func (w WString) Equals(w2 WType) WBool {
+	if v, ok := w2.(WString); ok {
+		return w == v
+	}
+	return false
+}
+
+func (w WString) String() string { return fmt.Sprintf("'%v'", string(w)) }
 
 // WBool is a boolean
 type WBool bool
 
 // IsZeroValue returns the zero value of a went boolean value
 func (w WBool) IsZeroValue() WBool { return !w }
-func (w WBool) String() string     { return fmt.Sprintf("%v", bool(w)) }
+
+// Equals checks if the type compared to is equal
+func (w WBool) Equals(w2 WType) WBool {
+	if v, ok := w2.(WBool); ok {
+		return w == v
+	}
+	return false
+}
+
+func (w WBool) String() string { return fmt.Sprintf("%v", bool(w)) }
 
 // WList is a list
-type WList []interface{}
+type WList []WType
 
 // IsZeroValue returns the zero value of a went list
 func (w WList) IsZeroValue() WBool { return len(w) == 0 }
-func (w WList) String() string     { return fmt.Sprintf("%v", []interface{}(w)) }
+
+// Equals checks if the type compared to is equal
+func (w WList) Equals(w2 WType) WBool {
+	v, ok := w2.(WList)
+	if !ok {
+		return false
+	} else if len(w) != len(v) {
+		return false
+	}
+	for i := 0; i < len(w); i++ {
+		if !w[i].Equals(v[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func (w WList) String() string { return fmt.Sprintf("%v", []WType(w)) }
 
 var (
 	tab        = "\t"
@@ -58,7 +111,7 @@ var (
 
 // Wmap is a naive implementation of a went "map" data structure
 // a data structure that maps strings to other values in wentlang
-type Wmap map[string]interface{}
+type Wmap map[string]WType
 
 func (w Wmap) toString(tabLevel int) string {
 	var buffer bytes.Buffer
@@ -84,4 +137,21 @@ func (w Wmap) toString(tabLevel int) string {
 
 // IsZeroValue returns the zero value of a went map
 func (w Wmap) IsZeroValue() WBool { return len(w) == 0 }
-func (w Wmap) String() string     { return w.toString(0) }
+
+// Equals checks if the type compared to is equal
+func (w Wmap) Equals(w2 WType) WBool {
+	map2, ok := w2.(Wmap)
+	if !ok {
+		return false
+	} else if len(w) != len(map2) {
+		return false
+	}
+	for k1, v1 := range w {
+		if !v1.Equals(map2[k1]) {
+			return false
+		}
+	}
+	return true
+}
+
+func (w Wmap) String() string { return w.toString(0) }
